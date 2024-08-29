4a7da42fa30028713aa2615c1cfde62af34200bc
gawr-fiude <eduardo.e.goto@gmail.com>
2024-08-29 13:23:47 -0300

    chore: implement the dynamic import

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script setup>
-import Eduardo from "./components/cards/Eduardo.vue";
-import John from "./components/cards/John.vue";
+import { defineAsyncComponent, shallowRef } from "vue";
+
+let cards = shallowRef([]);
+
+// Importing all .vue files from the cards folder
+Object.keys(import.meta.glob("./components/cards/*.vue", { eager: true }))
+  // We receive the whole path as a variable, which isn´t allowed to be passed on to import()
+  // https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
+  // Split the component path on / and .
+  // Get penultimate (file's name)
+  .map((card) => {
+    return card.split(/[\/ .]/).slice(-2)[0];
+  })
+  .forEach((card) => {
+    cards.value.push(
+      // Import Async Component
+      defineAsyncComponent(() => import(`./components/cards/${card}.vue`))
+    );
+  });
 </script>
 
 <template>
   <ul id="cards">
-    <Eduardo />
-    <John />
+    <component v-for="card in cards" :key="card" :is="card"></component>
   </ul>
 </template>
 
